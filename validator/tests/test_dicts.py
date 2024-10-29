d440f4c5303c2baa36f29cbd41fd39790ae53650
ikonnikovapv <spollyv@gmail.com>
2024-10-29 19:01:24 +0500

    add tests for DictSchema class and refactor tests for NumbersSchema

---
+++ validator/tests/test_dicts.py
@@ -0,0 +1,25 @@
+import pytest
+
+from validator.dicts import DictSchema
+from validator.validator import Validator
+
+@pytest.fixture(name="dct_sch")
+def get_schema():
+    return DictSchema()
+
+@pytest.fixture(name="v")
+def get_validator():
+    return Validator()
+
+
+class TestDictSchema:
+
+    def test_common(self,v, dct_sch):
+        dct_sch.shape({
+            'name': v.string().required(),
+            'age': v.number().positive(),
+        })
+        assert dct_sch.is_valid({'name': 'kolya', 'age': 100})
+        assert dct_sch.is_valid({'name': 'maya', 'age': None})
+        assert not dct_sch.is_valid({'name': '', 'age': None})
+        # assert not dct_sch.is_valid({'name': 'ada', 'age': -5})
